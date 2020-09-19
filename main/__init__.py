d2c09139870cb30eb4ca01e651493c99f832b02e
jod35 <jod35@test.com>
2020-09-19 17:08:40 +0300

    add[ authentication view for login]

--- main/__init__.py
+++ main/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask, jsonify
 from main.utils.database import db
 from main.api.views import api_bp
+from main.auth.views import auth_bp
 from main.config import Config
 from main.models.users import User
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+jwt = JWTManager(app)
 
 
-app.register_blueprint(api_bp, url_prefix='/')
+app.register_blueprint(api_bp, url_prefix='/api')
+app.register_blueprint(auth_bp, url_prefix='/auth')
 
 db.init_app(app)
 
--- main/auth/views.py
+++ main/auth/views.py
@@ -1,1 +1,36 @@
-=
+from flask_jwt_extended import create_access_token
+from flask import Blueprint, request, jsonify, make_response
+from main.models.users import User
+
+
+auth_bp = Blueprint('auth_bp', __name__)
+
+
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    username = data['username']
+
+    password = data['password']
+
+    user_to_login = User.query.filter_by(username=username).first()
+
+    if user_to_login and user_to_login.check_password(password):
+        token = create_access_token(identity=username)
+
+        return make_response(
+            jsonify({
+                "message": "User Logged In successfully",
+                "access_token": token,
+                "success": True
+            }), 200
+        )
+
+    else:
+        return make_response(
+            jsonify({
+                "message": "Invalid Login",
+                "success": False
+            }), 400
+        )
